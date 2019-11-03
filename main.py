932b2e940a87db5d61455ce500772b15dfe94b89
gabriel <gabrielchaperonb@gmail.com>
2019-11-03 20:14:26 -0300

    adds profit method to porfolio

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import random
+# import ipdb
 
 
 @dataclass
@@ -25,7 +26,7 @@ class Stock:
     x0: float = field(default_factory=partial(np.random.gamma, shape=50.))
     t0: datetime.datetime = field(default=datetime.datetime(2010, 1, 1))
     prices: np.ndarray = field(default_factory=lambda: np.array([]))
-    _w: np.ndarray = field(default_factory=lambda: np.array([]))
+    _b: np.ndarray = field(default_factory=lambda: np.array([]))
 
     # def __init__(self, mu=None, sigma=None, **kwargs):
     #     self.mu = mu if mu else np.random.normal(scale=2e-3)
@@ -49,8 +50,8 @@ class Stock:
         position in the array of prices based of off the initial date of the
         stock.
 
-        Lazily computes de values for the prices, which depend on the
-        "random walk" variable _w
+        Lazily computes de values for the prices, which depend on the random
+        variables stored in _b
         """
         if not isinstance(date, datetime.datetime):
             raise TypeError(
@@ -60,36 +61,86 @@ class Stock:
             raise Exception(f"Date shouldn't be less than {self.t0}")
 
         pos = int((date - self.t0) / datetime.timedelta(days=self.dt))
-        if len(self._w) < pos:
-            self._w = np.concatenate((
-                self._w,
+        if len(self._b) < pos:
+            self._b = np.concatenate((
+                self._b,
                 np.random.normal(
                     scale=np.sqrt(self.dt),
-                    size=2*pos  # calcular algunos extra, por que no
+                    size=(2*pos-len(self._b))  # calcular algunos extra
                 )
             ))
             step = np.exp((self.mu - self.sigma**2/2) * self.dt) \
-                * np.exp(self.sigma * self._w)
+                * np.exp(self.sigma * self._b)
             self.prices = self.x0 * step.cumprod()
-        return self.x0 if pos == 0 else self.prices[pos]
+
+        return self.x0 if pos == 0 else self.prices[pos-1]
+
+    def prices_range(self, date1, date2):
+        # ipdb.set_trace()
+        dt = datetime.timedelta(days=self.dt)
+        n = int((date2 - date1) / dt)
+
+        # print(n)
+        dates = (date1 + dt * i for i in range(n))
+        return np.array([self.price(date) for date in dates])
+
+
+class Portfolio:
+    def __init__(self, stocks=None):
+        if stocks is None:
+            self.stocks = [Stock() for i in range(random.randrange(5, 10))]
+        else:
+            if not all(isinstance(stock, Stock) for stock in stocks):
+                raise Exception("All stocks should be of type Stock")
+            self.stocks = stocks
+
+    def profit(self, date1, date2):
+        """
+        Return the profit of the portfolio between two dates as a percentage
+        """
+        if not isinstance(date1, datetime.datetime) or not isinstance(date2, datetime.datetime):
+            raise TypeError(
+                f"Dates should be of type datetime.datetime"
+            )
+        value1 = sum(stock.price(date1) for stock in self.stocks)
+        value2 = sum(stock.price(date2) for stock in self.stocks)
+
+        return (value2 - value1) / value1 * 100
+
+    def _show_graph(self, date1, date2):
+        # ipdb.set_trace()
+        p = self.stocks[0].prices_range(date1, date2)
+        for stock in self.stocks[1:]:
+            p += stock.prices_range(date1, date2)
+        plt.plot(p)
+        plt.show()
 
 
 def main():
-    s = Stock()
-    tic = time.time()
-    s.price(datetime.datetime(2011,1,1))
-    toc = time.time()
-    print(f"primera ejecucion {toc-tic}s")
+    # s = Stock()
+    # tic = time.time()
+    # print(s.price(datetime.datetime(2010, 1, 2)))
+    # toc = time.time()
+    # print(f"primera ejecucion {toc-tic}s")
+    # s._show_graph()
 
-    times = []
-    for i in range(20):
-        tic = time.time()
-        s.price(datetime.datetime(random.randrange(2010, 2100), 1, 1))
-        toc = time.time()
-        times.append(toc-tic)
-    print(f"promedio de 20 ejecuciones posteriores {np.mean(times)}s")
+    # times = []
+    # for i in range(20):
+    #     tic = time.time()
+    #     s.price(datetime.datetime(random.randrange(2010, 2100), 1, 1))
+    #     toc = time.time()
+    #     times.append(toc-tic)
+    # print(f"promedio de 20 ejecuciones posteriores {np.mean(times)}s")
 
-    s._show_graph()
+
+    # s.price(datetime.datetime(2010, 4, 1))
+    # s._show_graph()
+
+    p = Portfolio()
+    d1 = datetime.datetime(2018, 1, 1)
+    d2 = datetime.datetime(2019, 1, 1)
+    print(f"ganancia de {p.profit(d1, d2)}%")
+    p._show_graph(d1, d2)
 
 
 if __name__ == '__main__':
